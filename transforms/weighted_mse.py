b9a7dfbbf04b136c151d20a59fc59714256ce685
Keno <37253540+kbressem@users.noreply.github.com>
2022-10-24 11:54:07 +0200

    Delete weighted_mse.py

--- transforms/weighted_mse.py
+++
@@ -1,8 +0,0 @@
-class WeightedMSELoss: 
-  
-  def __init__(self, scale_factor): 
-    self.scale_factor = scale_factor
-    
-  def __call__(self, input, target): 
-    weigth = (target + 1) ** self.scale_factor
-    
